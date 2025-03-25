337f7e8a387ddc795f0361ea479beabc384e6b82
madhtl <s28795@pjwstk.edu.pl>
2025-03-25 21:43:29 +0100

    Chore: Functionality added

--- HiddenMarkovModel.py
+++ HiddenMarkovModel.py
@@ -7,6 +7,7 @@ class HiddenMarkovModel():
 
     def play(self, rounds=100):
         last_move = None
+        won_games = {1: 0, 0: 0, -1: 0}
         for r in range(rounds):
             opponent_move = random.choice([0,1,2])
             print(self.states[opponent_move])
@@ -14,10 +15,16 @@ class HiddenMarkovModel():
                 # [from][to] - [row][column]
                 self.transition_matrix[last_move][opponent_move] += 1
                 # print(self.transition_matrix)
+            if last_move is not None:
+                prediction = self.__predict(self.transition_matrix, last_move)
+                print(f"Prediction: {prediction}")
+                result = self.determine_result(prediction, self.states[last_move])
+                print(f"Round result: {result}")
+                won_games[result] += 1
             last_move = opponent_move
-        return self.transition_matrix, last_move
+        return self.transition_matrix, last_move, won_games
 
-    def predict(self, transition_matrix, last_move : int) -> str:
+    def __predict(self, transition_matrix, last_move: int) -> str:
         state_index = last_move
         row_sum = np.sum(transition_matrix[state_index])
         if row_sum > 0:
@@ -28,9 +35,22 @@ class HiddenMarkovModel():
             print(f'maxProb: {max_prob:.4f} for state: {predicted_state}')
             return predicted_state
 
+    def determine_result(self, predicted_move, last_move):
+        result_map = {
+            ("R", "R"): 0,
+            ("R", "Pr"): 1,
+            ("R", "Sc"): -1,
+            ("Pr", "R"): -1,
+            ("Pr", "Pr"): 0,
+            ("Pr", "Sc"): 1,
+            ("Sc", "R"): 1,
+            ("Sc", "Pr"): -1,
+            ("Sc", "Sc"): 0
+        }
+        return result_map.get((predicted_move, last_move))
+
 game = HiddenMarkovModel()
-tm, lm = game.play()
+tm, lm, wg = game.play()
 print("\nFinal Transition Matrix:")
 print(game.transition_matrix)
-predicted_move = game.predict(tm, lm)
-print(f"Predicted next move is {predicted_move}")
+print(f"\n{wg}")
